56df742e2b993a7433775c305e39341b44f8a8c9
nitecse <hitman12457868@gmail.com>
2024-09-10 13:42:17 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!firstName || !lastName) { console.log("not submitted"); return; }
+    setFullName(firstName + " " + lastName);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <h5>First Name: </h5>
+        <input onChange={(e) => setFirstName(e.target.value)}></input>
+        <h5>Last Name: </h5>
+        <input onChange={(e) => setLastName(e.target.value)}></input>
+        <br />
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
+      <h5>Full name: {fullName}</h5>
     </div>
   );
 }
